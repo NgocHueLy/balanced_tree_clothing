7aea7ac061cafefbfd2651677c2a8f7fa7ea26d5
unknown <kyotakaspiral@gmail.com>
2024-03-20 14:34:48 +0700

    update models

--- balanced_tree/models/staging/stg_sales.sql
+++ balanced_tree/models/staging/stg_sales.sql
@@ -4,12 +4,38 @@
     )
 }}
 
+WITH sales AS (
+    SELECT *
+    FROM {{ ref("base_sales") }}
+),
+product_detail AS (
+    SELECT *
+    FROM {{ ref("base_product_details") }}
+),
+date AS (
+    SELECT *
+    FROM {{ ref("dim_dates") }}
+)
 
-SELECT * 
-FROM {{ source('staging', 'sales') }}
-
-{% if var('is_test_run', default=true) %}
-
-  limit 100
-
-{% endif %}
+SELECT
+    sales.prod_id AS product_id,
+    product_detail.product_name AS product_name,
+    product_detail.style_name AS style_name,
+    product_detail.segment_name AS segment_name,
+    product_detail.category_name AS category_name,
+    sales.qty AS quantity,
+    sales.price AS price,
+    sales.discount AS discount_percentage,
+    sales.qty * sales.price AS revenue,
+    sales.member AS member,
+    sales.txn_id AS transaction_id,
+    sales.start_txn_time AS transaction_datetime,
+    date.month,
+    date.month_name,
+    date.weekday_name,
+    date.calendar_week
+FROM sales
+INNER JOIN product_detail
+ON sales.prod_id = product_detail.product_id
+INNER JOIN date
+ON sales.transaction_date = date.date
